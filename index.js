364473de060b81ea64312ad400edd46f5171b596
Ian Frankel <ian.frankel@gmail.com>
2023-09-12 00:45:46 -0400

    employeed can update

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+//Write your solution in this file!
+let employee = { name: "Horatio", streetAddress: "1600 Pennsylvania Ave" };
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    let employeeCopy = {...employee}
+    employeeCopy[key] = value;
+    return employeeCopy
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let employeeCopy = {...employee};
+    delete employeeCopy[key];
+    console.log(employeeCopy)
+    return employeeCopy
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
